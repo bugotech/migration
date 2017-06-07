a5c30b6a6f5f0c90129e78e85e09a6ad579e2c37
Bruno Goncalves <bugotech@gmail.com>
2017-06-07 02:41:10 -0300

    campos pelo call

--- src/Schema/MigrationCommands.php
+++ src/Schema/MigrationCommands.php
@@ -1,5 +1,7 @@
 <?php namespace Bugotech\Migration\Schema;
 
+use Illuminate\Support\Str;
+
 trait MigrationCommands
 {
     /**
@@ -22,10 +24,22 @@ trait MigrationCommands
         $this->drop($tableName);
     }
 
-    public function createFieldString($tableName, $nome, $len)
+    /**
+     * @param $name
+     * @param $arguments
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
     {
-        $this->table($tableName, function(Table $table) use($nome, $len) {
-            $table->string($nome, $len);
-        });
+        if (Str::is('createField*', $name)) {
+            $name = strtolower(str_replace('createField', '', $name));
+            $tableName = $arguments[0];
+            array_shift($arguments);
+            $this->table($tableName, function(Table $table) use($name, $arguments) {
+                call_user_func_array([$table, $name], $arguments);
+            });
+        }
+
+        throw new \Exception(sprintf('Method %s not found in the class %s', $name, get_class($this)));
     }
 }
