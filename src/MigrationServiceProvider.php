08899fe0b56899fbd5bf595de0a2dd8acb09155d
Bruno Goncalves <bugotech@gmail.com>
2017-06-07 02:58:21 -0300

    migrator

--- src/MigrationServiceProvider.php
+++ src/MigrationServiceProvider.php
@@ -6,6 +6,23 @@ use Bugotech\Migration\Console\MigrateCommand;
 class MigrationServiceProvider extends \Illuminate\Database\MigrationServiceProvider
 {
     /**
+     * Register the migrator service.
+     *
+     * @return void
+     */
+    protected function registerMigrator()
+    {
+        // The migrator is responsible for actually running and rollback the migration
+        // files in the application. We'll pass in our database connection resolver
+        // so the migrator can resolve any of these connections when it needs to.
+        $this->app->singleton('migrator', function ($app) {
+            $repository = $app['migration.repository'];
+
+            return new Migrator($repository, $app['db'], $app['files']);
+        });
+    }
+
+    /**
      * Register all of the migration commands.
      *
      * @return void
